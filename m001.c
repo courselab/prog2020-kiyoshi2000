e56f09dabc2207aaaaa2f71e54b660b496efe9b3
kiyoshi2000 <50682796+kiyoshi2000@users.noreply.github.com>
2020-06-21 20:33:56 -0300

    Update m001.c

--- m001.c
+++ m001.c
@@ -24,7 +24,12 @@
 
 int greatest (int a, int b, int c)
 {
+   if (a<b && a<c)
   return a;
+   else if (b<a && b<c)
+      return b;
+   else if (c<a && c<b)
+      return c;
 }
 
 /* Do not edit this function. */
