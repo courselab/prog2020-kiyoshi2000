c97b52d9f7c42282063b860b460280c4bfd39e5b
KiyoshiAraki <kiyoshi.araki@hotmail.com>
2020-07-07 15:32:41 +0000

    009/008

--- m008.c
+++ m008.c
@@ -29,15 +29,41 @@
 
 /* Converts the string 's' from first-middle-last name 
    form into LAST-first-middle form. */
-
 void lastname (char *s)
 {
-  
+int n;
 
-  
+char x[MAX]; 
+char y[MAX]; 
+char z[MAX];
+char w[MAX];
+
+n= strlen(s);
+
+int i;
+int j;
+
+i=0;
+j=0;
+
+while (s[n-1-j]!= 32) //Ler de tras para frente ate pegar um 'espaco'
+j++;
+for (i=0; i<j-1; i++){
+{if ((s[n-j+i] >=97) && (s[n-j+i]<=122)) //Se estiver minuscula 
+x[i]=(s[n-j+i]-32); // Vira Maiscula
+else
+x[i]=s[n-j+i];} //Se tiver Maiscula continua maiscula
+} //Colocar na string x a ultima palavra em maisculo
+strcpy(y,x);  //Guardar a ultima palavra em maisculo em uma string y 
+
+for(i=0; i<=n-j-1; i++)//Ler ate o ultimo espaco 
+z[i]=s[i]; //Printar o codigo normal ate o ultimo espaco
+strcpy(w, z); //Guardar o codigo normal em uma string w
+
+strcpy(s, y);
+strcat(s, ", ");
+strcat(s, w);
 }
-
-
 
 /* Do not edit this function. */
 
